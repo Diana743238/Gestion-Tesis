de09389d695d9f9b93f624c3b684af8ee19b43fc
Luiyi Antony Quispe Palomino <64095671+blablabla115@users.noreply.github.com>
2021-02-06 21:29:34 -0500

    agregando formulario para proveido

---
+++ Proyecto/LibFormularios/FrmProveido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LibFormularios
+{
+    public partial class FrmProveido : LibFormularios.FrmPadre
+    {
+        public FrmProveido()
+        {
+            InitializeComponent();
+        }
+    }
+}
